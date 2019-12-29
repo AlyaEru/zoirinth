c38ad97e2351a10855fbf7a4584206c4cacf2e49
Tim Schwab <timothy.john.schwab@gmail.com>
2019-12-29 17:22:35 -0500

    refactor simulateMap.js

--- src/simulateMap.js
+++ src/simulateMap.js
@@ -3,6 +3,8 @@ const renderMap = require('./renderMap')
 const actions = require('./actions')
 const util = require('./utilities')
 
+let mazeWidth, mazeHeight
+
 function getMapSimulation(map, ...entities) {
 	mapCopy = JSON.parse(JSON.stringify(map)) //deep clone map
 	for (let entity of entities) {
@@ -13,66 +15,16 @@ function getMapSimulation(map, ...entities) {
 	return mapCopy
 }
 
-function initializeMap(
-	player,
-	numClovers,
-	numZoids,
-	mazeWidth,
-	mazeHeight,
-	level
-) {
+function initializeMap(player, numClovers, numZoids, width, height, level) {
+	mazeWidth = width
+	mazeHeight = height
+
 	let map = buildMaze.build(
 		mazeWidth,
 		mazeHeight,
 		0.5,
 		1 - ((level - 1) % 5) / 4
 	)
-
-	let takenPoints = []
-	function randSpawnPoint() {
-		while (true) {
-			point = {
-				x: util.rand(mazeWidth) * 2 + 1,
-				y: util.rand(mazeHeight) * 2 + 1
-			}
-			let match = false
-			for (takenPoint of takenPoints) {
-				if (takenPoint.x == point.x && takenPoint.y == point.y) {
-					match = true
-				}
-			}
-			if (match == false) {
-				takenPoints.push(point)
-				return point
-			}
-		}
-	}
-	function createZoids(numZoids) {
-		let zoids = []
-
-		for (let i = 0; i < numZoids; i++) {
-			zoids.push({
-				loc: randSpawnPoint(),
-				getType: () => 'zoid',
-				clovers: 0,
-				actionQueue: [],
-				mode: '',
-				dir: ''
-			})
-			zoids[i].actionQueue.push(actions.zoidAction(zoids[i]))
-		}
-		return zoids
-	}
-	function createClovers(numClovers) {
-		let clovers = []
-		for (let i = 0; i < numClovers; i++) {
-			clovers.push({
-				loc: randSpawnPoint(),
-				getType: () => 'clover'
-			})
-		}
-		return clovers
-	}
 
 	zoids = createZoids(numZoids)
 	clovers = createClovers(numClovers)
@@ -81,6 +33,55 @@ function initializeMap(
 
 	renderMap.launch(getMapSimulation(map, [player], zoids, clovers)) //first time launching the game board and setting the table width and height
 	return map
+}
+
+function randSpawnPoint() {
+	let takenPoints = []
+	while (true) {
+		point = {
+			x: util.rand(mazeWidth) * 2 + 1,
+			y: util.rand(mazeHeight) * 2 + 1
+		}
+		let match = false
+		for (takenPoint of takenPoints) {
+			if (takenPoint.x == point.x && takenPoint.y == point.y) {
+				match = true
+			}
+		}
+		if (match == false) {
+			takenPoints.push(point)
+			return point
+		}
+	}
+}
+
+function createZoids(numZoids) {
+	let zoids = []
+
+	for (let i = 0; i < numZoids; i++) {
+		zoids.push({
+			loc: randSpawnPoint(),
+			getType: () => 'zoid',
+			clovers: 0,
+			actionQueue: [],
+			mode: '',
+			dir: ''
+		})
+		zoids[i].actionQueue.push(actions.zoidAction(zoids[i]))
+	}
+	return zoids
+}
+
+function createClovers(numClovers) {
+	let clovers = []
+
+	for (let i = 0; i < numClovers; i++) {
+		clovers.push({
+			loc: randSpawnPoint(),
+			getType: () => 'clover'
+		})
+	}
+	return clovers
 }
 
 module.exports = {
