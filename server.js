0d799a1dd0446b442bd0f7a6cfa5ed7d1840df2c
danschwab <dschwabdesign@gmail.com>
2026-07-29 07:44:30 -0400

    serversiide api for highscores and deployment config

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const PORT = 3001
+
+app.use(express.json())
+app.use(express.static('public'))
+
+// GET /highscores - retrieve highscores
+app.get('/highscores', (req, res) => {
+	fs.readFile('./highscores', 'utf8', (err, data) => {
+		if (err) {
+			res.status(500).send('Error reading highscores')
+			return
+		}
+		res.send(data)
+	})
+})
+
+// POST /highscores - add new highscore
+app.post('/highscores', (req, res) => {
+	const {username, score, level, date} = req.body
+	if (!username || !score || !level || !date) {
+		res.status(400).send('All fields required')
+		return
+	}
+	const highscoreEntry = JSON.stringify({username, score, level, date}) + '\n'
+	fs.appendFile('./highscores', highscoreEntry, err => {
+		if (err) {
+			res.status(500).send('Error writing highscore')
+			return
+		}
+		res.send('Highscore saved')
+	})
+})
+
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
