0b483b31525538a955371689eec56e658129c756
danschwab <dschwabdesign@gmail.com>
2026-07-31 15:48:36 -0400

    npm & webpack in frontend only

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,6 +6,6 @@ module.exports = {
 	entry: ['./src/main.js'],
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(__dirname, './dist')
+		path: path.resolve(__dirname, './public/dist')
 	}
 }
