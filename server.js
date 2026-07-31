e22397e71656cd74252696f590f1d053eaab1dbf
danschwab <dschwabdesign@gmail.com>
2026-07-30 22:40:07 -0400

    server and port configs, potentially jenky, I'm not great at this

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const app = express()
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.use(express.json())
 app.use(express.static('public'))
