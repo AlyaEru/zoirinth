7996a8cd3de97b5ba22f9f950103139fcff12bc4
danschwab <dschwabdesign@gmail.com>
2026-07-31 16:42:45 -0400

    fix server errors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const fs = require('fs')
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT || 3001
 
@@ -8,32 +9,34 @@ app.use(express.static('public'))
 
 // GET /highscores - retrieve highscores
 app.get('/highscores', (req, res) => {
-	fs.readFile('./highscores', 'utf8', (err, data) => {
-		if (err) {
-			res.status(500).send('Error reading highscores')
-			return
-		}
-		res.send(data)
-	})
+  const highscoresPath = path.join(__dirname, 'highscores')
+  fs.readFile(highscoresPath, 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading highscores')
+      return
+    }
+    res.send(data)
+  })
 })
 
 // POST /highscores - add new highscore
 app.post('/highscores', (req, res) => {
-	const {username, score, level, date} = req.body
-	if (!username || !score || !level || !date) {
-		res.status(400).send('All fields required')
-		return
-	}
-	const highscoreEntry = JSON.stringify({username, score, level, date}) + '\n'
-	fs.appendFile('./highscores', highscoreEntry, err => {
-		if (err) {
-			res.status(500).send('Error writing highscore')
-			return
-		}
-		res.send('Highscore saved')
-	})
+  const {username, score, level, date} = req.body
+  if (!username || !score || !level || !date) {
+    res.status(400).send('All fields required')
+    return
+  }
+  const highscoresPath = path.join(__dirname, 'highscores')
+  const highscoreEntry = JSON.stringify({username, score, level, date}) + '\n'
+  fs.appendFile(highscoresPath, highscoreEntry, err => {
+    if (err) {
+      res.status(500).send('Error writing highscore')
+      return
+    }
+    res.send('Highscore saved')
+  })
 })
 
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
